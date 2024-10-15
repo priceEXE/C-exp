37aa27ca6b9823e39a53c2c85ec4a279d745328b
priceEXE <152944571+priceEXE@users.noreply.github.com>
2024-10-15 23:02:57 +0800

    实现一个测试集下的Execute

--- src/ExecutorImpl.cpp
+++ src/ExecutorImpl.cpp
@@ -14,6 +14,15 @@ namespace adas{
     }
 
     void ExecutorImpl::Execute(const std::string &commands) noexcept {
-        
+        for(const auto cmd : commands)
+        {
+            if( cmd == 'M' )
+            {
+                if(pose.heading == 'E') { ++pose.x; }
+                else if(pose.heading == 'W') { --pose.x; }
+                else if(pose.heading == 'N') { ++pose.y; }
+                else if(pose.heading == 'S') { --pose.y; }
+            }
+        }
     }
 }
